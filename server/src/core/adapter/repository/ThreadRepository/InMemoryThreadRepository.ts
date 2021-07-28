8f75e35aea4a154a6e37b3ad602a09d62050fd8d
Sho Sakai <ropitaru88@gmail.com>
2021-07-29 00:37:55 +0900

    fix(server): InMemoryThreadRepositoryを修正

--- server/src/core/adapter/repository/ThreadRepository/InMemoryThreadRepository.ts
+++ server/src/core/adapter/repository/ThreadRepository/InMemoryThreadRepository.ts
@@ -4,6 +4,14 @@ import IThreadReporsitory from './IThreadRepository'
 export default class InMemoryThreadRepository implements IThreadReporsitory {
   private threadList: Thread[] = []
 
+  getById(id: string): Promise<Thread> {
+    return new Promise((resolve) => {
+      const thread = this.threadList.find((thread) => thread.id === id)
+      if (!thread) throw new Error('Thread not found')
+      resolve(thread)
+    })
+  }
+
   save(thread: Thread): Promise<Thread> {
     return new Promise((resolve) => {
       this.threadList.push(thread)
